4a1f41c66b05aef1421a46cdaa826021d5c6eba8
Marianna Kurtukova <marianna.kurtukova@jetbrains.com>
2022-03-28 19:03:43 +0300

    IIS regression

---
+++ IISRegression/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace IISRegression.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            var str = "Hello " + name;
+            ViewData["Message"] = str;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
